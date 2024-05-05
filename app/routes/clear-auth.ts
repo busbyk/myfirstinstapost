e0a5199cc6924c9b9191a299c389434e656805f4
Kellen Busby <kellenbusby@gmail.com>
2024-05-04 18:41:44 -0700

    clear-auth route

---
+++ app/routes/clear-auth.ts
@@ -0,0 +1,23 @@
+import { LoaderFunctionArgs, redirect } from '@remix-run/node';
+import { kv } from '@vercel/kv';
+import { destroySession, getSession } from '~/auth';
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const session = await getSession(request.headers.get('Cookie'));
+  const auth = session.get('auth');
+
+  if (!auth) {
+    // TODO session flash?
+    return redirect('/');
+  }
+
+  const user = JSON.parse(auth);
+
+  await kv.set(user.id, null);
+
+  return redirect('/', {
+    headers: {
+      'Set-Cookie': await destroySession(session),
+    },
+  });
+}
